bd8ba6e85cf2549f4b8af292f0c24182d9eb6310
Hans <hans@hansl.ca>
2018-05-30 11:53:16 -0700

    feat(@angular-devkit/core): add new hosts
    SafeHost swallows all errors with values.
    EmptyHost has no files/directories.

--- packages/angular_devkit/core/src/virtual-fs/host/index.ts
+++ packages/angular_devkit/core/src/virtual-fs/host/index.ts
@@ -8,9 +8,12 @@
 
 export * from './alias';
 export * from './buffer';
+export * from './empty';
 export * from './interface';
 export * from './memory';
 export * from './pattern';
+export * from './record';
+export * from './safe';
 export * from './scoped';
 export * from './sync';
 
